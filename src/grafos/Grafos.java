2e691be9e1a7f63a341975cdf097c2ad58fdad0d
Ítalo Gama <itrgama@gmail.com>
2019-05-14 15:58:45 -0300

    matriz de incidencia concluida

--- src/grafos/Grafos.java
+++ src/grafos/Grafos.java
@@ -7,6 +7,7 @@ package grafos;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
@@ -27,14 +28,14 @@ public class Grafos {
         int numeroVertices = grafos.numeroVertices(txt);
         int numeroArestas = grafos.numeroArestas(txt);
         int matrizAdjacencia[][] = grafos.matrizAdjacencia(txt, numeroVertices);
+        int matrizIncidencia[][] = grafos.matrizIncidencia(txt, numeroVertices, numeroArestas);
+
         String matrizAdjacenciaString = "";
+        String matrizIncidenciaString = "";
 
-        /* Exibindo Número de Vértices */
-        System.out.println("Número de Vertices: " + numeroVertices);
-        System.out.println("\n");
 
-        /* Exibindo Número de Arestas */
-        System.out.println("Número de Arestas: " + numeroArestas);
+        /* Exibindo Ordem e Tamanho */
+        Grafos.ordemTamanhoGrafo(numeroVertices, numeroArestas);
         System.out.println("\n");
 
         /* Exibindo MatrizAdjacencia */
@@ -52,14 +53,34 @@ public class Grafos {
         grauDosVertices(numeroVertices, matrizAdjacencia);
 
         /* Exibindo A Lista de Adjacencia */
-        grafos.listaAdjacencia(numeroVertices, matrizAdjacencia);
+        Grafos.listaAdjacencia(numeroVertices, matrizAdjacencia);
+
+        /* Exibindo A Matriz de Indêcnia */
+        System.out.println("Matriz de Incidência:");
+        for (int i = 1; i <= numeroArestas; i++) {
+            for (int j = 1; j <= numeroVertices; j++) {
+                matrizIncidenciaString += " " + matrizIncidencia[i][j] + " ";
+            }
+            matrizIncidenciaString += ("\n");
+        }
+        System.out.println(matrizIncidenciaString);
+        System.out.println("\n");
+
+        /* Exibindo Se O Grafo é Simples, Pseudografo ou Multigrafo */
+        //grafos.simplesPseudoMulti(numeroVertices, matrizAdjacencia);
+    }
+
+    public static void ordemTamanhoGrafo(int numeroVertices, int numeroArestas) {
+
+        System.out.println("A Ordem do Grafo é: " + numeroVertices);
+        System.out.println("O Tamanho do Grafo é: " + numeroArestas);
 
     }
 
     public int numeroVertices(String txt) {
 
         int numeroVertices = 0;
-        String[] caracteres = null;
+        String[] caracteres;
         String lerLinha;
 
         try {
@@ -83,7 +104,7 @@ public class Grafos {
                 }
 
             } while (lerLinha != null);
-        } catch (Exception e) {
+        } catch (IOException | NumberFormatException e) {
             System.out.println("ERROR: " + e);
         }
         return numeroVertices;
@@ -92,7 +113,7 @@ public class Grafos {
     public int numeroArestas(String txt) {
 
         int numeroArestas = 0;
-        String[] caracteres = null;
+        String[] caracteres;
         String lerLinha;
 
         try {
@@ -109,17 +130,17 @@ public class Grafos {
                 }
 
             } while (lerLinha != null);
-        } catch (Exception e) {
+        } catch (IOException e) {
             System.out.println("ERROR: " + e);
         }
         return numeroArestas;
     }
 
-    public int[][] matrizAdjacencia(String txt, int vertices) {
+    public int[][] matrizAdjacencia(String txt, int numeroVertices) {
 
-        String[] caracteres = null;
-        int[][] m = new int[vertices + 1][vertices + 1];
-        String Lerlinha;
+        String[] caracteres;
+        int[][] m = new int[numeroVertices + 1][numeroVertices + 1];
+        String lerLinha;
 
         try {
 
@@ -128,11 +149,11 @@ public class Grafos {
 
             do {
 
-                Lerlinha = d.readLine();
+                lerLinha = d.readLine();
 
-                if (Lerlinha != null) {
+                if (lerLinha != null) {
 
-                    caracteres = Lerlinha.split(">");
+                    caracteres = lerLinha.split(">");
 
                     int linha = Integer.parseInt(caracteres[0]);
                     int coluna = Integer.parseInt(caracteres[1]);
@@ -141,8 +162,8 @@ public class Grafos {
 
                 }
 
-            } while (Lerlinha != null);
-        } catch (Exception e) {
+            } while (lerLinha != null);
+        } catch (IOException | NumberFormatException e) {
             System.out.println("ERROR: " + e);
         }
 
@@ -183,6 +204,64 @@ public class Grafos {
         }
 
         System.out.println(listaAdjacencia);
+
+    }
+
+    public int[][] matrizIncidencia(String txt, int vertices, int arestas) {
+
+        int linhaMatriz = 1;
+        int[][] matrizIncidencia = new int[arestas + 1][vertices + 1];
+        String lerLinha;
+        String[] caracteres;
+
+        try {
+
+            FileInputStream arquivo = new FileInputStream(txt);
+            BufferedReader d = new BufferedReader(new InputStreamReader(arquivo));
+
+            do {
+
+                lerLinha = d.readLine();
+
+                if (lerLinha != null) {
+
+                    caracteres = lerLinha.split(">");
+
+                    int tempUm = Integer.parseInt(caracteres[0]);
+                    int tempDois = Integer.parseInt(caracteres[1]);
+
+                    matrizIncidencia[linhaMatriz][tempUm] = 1;
+                    matrizIncidencia[linhaMatriz][tempDois] = 1;
+
+                }
+
+                linhaMatriz++;
+
+            } while (lerLinha != null);
+        } catch (IOException | NumberFormatException e) {
+            System.out.println("ERROR: " + e);
+        }
+
+        return matrizIncidencia;
+
+    }
+
+    public static void simplesPseudoMulti(int numeroVertices, int[][] matrizAdjacencia) {
+
+        int tipo = 0;
+        int diagonalDaMatriz = 0;
+
+        for (int i = 1; i <= numeroVertices; i++) {
+            diagonalDaMatriz += matrizAdjacencia[i][i];
+        }
+
+        if (diagonalDaMatriz == 0) {
+            System.out.println("O Grafo é Simples!");
+        }
+
+        if (diagonalDaMatriz > 0) {
+            System.out.println("O Grafo é Pseudografo, possui laço. A soma da Diagonal da Matriz de Adjacência é:" + diagonalDaMatriz);
+        }
 
     }
 
